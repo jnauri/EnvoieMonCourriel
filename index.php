4e072254774e073f5016c4bb106d1d059c4a3b92
Machou <dokcharlie@gmail.com>
2016-10-18 03:19:19 +0200

    1er commit, readme.txt + inex.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+	<meta charset="utf-8" />
+
+	<title>Envoie Mon Courriel</title>
+</head>
+
+<body>
+	<h1>Envoie Mon Courriel</h1>
+
+	<p>Envoie Mon Courriel est un petit site pour envoyer vos courriels gratuitement.</p>
+
+	<form action="courriel.php" method="post">
+		<label for="courriel">Quel Courriel</label>
+		<input type="text" name="courriel" placeholder="Ex: moi@gmail.com" />
+
+		<label for="message">Mon Message</label>
+		<textarea name="message" placeholder="Ex: Bonjour, ...blabla..., Bonne soirée"></textarea>
+
+		<input type="submit">J'envoie</input>
+	</form>
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Bonjour
+
+Le Script Envoie Mon Courriel est un test pour le cours "Gérez votre code avec Git et GitHub" sur OpenClassrooms.com
+
+Url : https://openclassrooms.com/courses/gerer-son-code-avec-git-et-github
+
+
+Ce script servira à envoyer des courriels très simplement et "sobrement" en utilisant le langage serveur PHP.
+
+Hihi
